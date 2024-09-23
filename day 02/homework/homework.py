8d89d27453b185c960bf7f8a16fc14341ec477d2
Andriaebralidze <ebralidzeandria90@gmail.com>
2024-09-23 11:10:03 +0400

    second commit

---
+++ day 02/homework/homework.py
@@ -0,0 +1,7 @@
+print("andria ebralidze")
+
+print("GOA aris sauketeso programirebis skola")
+
+print(30 + 20)
+
+print("30 + 20")
---
+++ day 03/homework/homework.py
@@ -0,0 +1,22 @@
+number=13
+
+age=25
+
+name="andria"
+
+surname="ebraidze"
+
+credit = 300
+
+my_name = "andria"
+
+my_brothet_name = 'aleqsi'
+
+price = 50
+
+color ="red"
+
+message = "andria magaria"
+
+
+
