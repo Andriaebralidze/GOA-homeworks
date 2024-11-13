26284d0d4122b343d3f7dd5d711a566275ebd026
aleqsandreebralidze <aleqsiko72@gmail.com>
2024-11-13 10:37:40 +0400

    fourthinth commit

---
+++ level 03/homework/classwork.py
@@ -0,0 +1,12 @@
+# 4) დაადეკლარირეთ 10 ცვლადი და ყველა ცვლადს მიეცით განსხვავებული სახელი და მნიშვნელობა, რომლებიც აუცილებლად უნდა შეესაბამებოდეს
+
+animals = "lion"
+food  = "pizza"
+boots = "Nike mercurial"
+game = "brawl stars"
+sports = "football"
+num1 = 23
+name = "andria"
+phone = "iphone 12"
+monitor = "samsung"
+city = "Tbilisi"
